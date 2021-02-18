76b19c7788dd86ca83a633d9d7d640e7678394a5
unknown <7500776802>
2021-02-18 17:19:38 +0530

    Updates

---
+++ precache-manifest.dd7e4b92cb69c032898b6b3c263f7d40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82404551c1cf5995bb28466491a88a2e",
+    "url": "/Rohandeshwal/spacexSapient.git/index.html"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/Rohandeshwal/spacexSapient.git/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4330841e8c07f7b1a3eb",
+    "url": "/Rohandeshwal/spacexSapient.git/static/css/main.e141296e.chunk.css"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/Rohandeshwal/spacexSapient.git/static/js/2.6cdbf0dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Rohandeshwal/spacexSapient.git/static/js/2.6cdbf0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4330841e8c07f7b1a3eb",
+    "url": "/Rohandeshwal/spacexSapient.git/static/js/main.276fb9c3.chunk.js"
+  },
+  {
+    "revision": "5d007b56ac14e52bf44d",
+    "url": "/Rohandeshwal/spacexSapient.git/static/js/runtime-main.751acdbe.js"
+  },
+  {
+    "revision": "c537d7d180515a2d14e723821dcf5097",
+    "url": "/Rohandeshwal/spacexSapient.git/static/media/loadRocket.c537d7d1.gif"
+  }
+]);
